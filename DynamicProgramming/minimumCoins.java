ac8165dd368224d9b72f3e8e80f89f24e74a93c5
Akash <akashsrivastava972002@gmail.com>
2023-01-26 16:13:13 +0530

    Removed Sexy

--- DynamicProgramming/minimumCoins.java
+++ DynamicProgramming/minimumCoins.java
@@ -31,6 +31,5 @@ public class minimumCoins {
         int[] numCoins = new int[18];
         Arrays.fill(numCoins, -1);
         System.out.println(minCoins(n, coins, numCoins));
-        System.out.println("sexy");
     }
 }
